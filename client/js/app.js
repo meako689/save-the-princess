54ea1e951ae9552e11441f9d286dfb770735ecd4
UnforbiddenYet <dmytro.lubenets@gmail.com>
2014-09-13 22:13:04 +0300

    app sceleton

--- client/js/app.js
+++ client/js/app.js
@@ -1,21 +1,152 @@
-// Ionic Starter App
+window.app = angular.module('app', ['ui.router', 'restangular', 'appControllers']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+app.config(function ($stateProvider, $urlRouterProvider, RestangularProvider) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
-      StatusBar.styleDefault();
-    }
-  });
+//  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+
+    // HOME STATES AND NESTED VIEWS ========================================
+    .state('home', {
+      url: '/home',
+      templateUrl: 'partials/index.html'
+    })
+
+    // MALE VIEWS //
+
+    .state('challenges', {
+      url: '/male/challenges',
+      templateUrl: 'partials/male/challenges.html'
+    })
+
+    .state('challenge', {
+      url: '/male/challenge/:chId',
+      templateUrl: 'partials/male/challenge.html'
+    })
+
+    .state('challenge.currentMale', {
+      url: '/male/challenges/current',
+      templateUrl: 'partials/male/challenge.current.html'
+    })
+
+    .state('challenge.finishedMale', {
+      url: '/male/challenges/finished',
+      templateUrl: 'partials/male/challenge.finished.html'
+    })
+
+    // FEMALE VIEWS //
+
+    .state('profile', {
+      url: '/female/main/',
+      templateUrl: 'partials/female/profile.html'
+    })
+
+    .state('profile.firstTime', {
+      url: '/female/profile/:profileId/first-time',
+      templateUrl: 'partials/female/profile.first-time.html'
+    })
+
+    .state('challenge.personalityFemale', {
+      url: '/female/challenge/',
+      templateUrl: 'partials/female/challenge.personality.html'
+    })
+
+    .state('challenge.stepsFemale', {
+      url: '/female/challenge/steps',
+      templateUrl: 'partials/female/challenge.steps.html'
+    })
+
+    .state('challenge.currentFemale', {
+      url: '/female/challenge/current',
+      templateUrl: 'partials/female/challenge.current.html'
+    })
+
+    .state('challenge.finishedFemale', {
+      url: '/female/challenges/finished',
+      templateUrl: 'partials/female/challenge.finished.html'
+    });
+
+
+  var mainPageUrl = "/home";
+
+  $urlRouterProvider.otherwise(mainPageUrl);
+
+//      var config = window.vms ? window.vms.config : null;
+//      if (config) {
+//        $restangularProvider.setBaseUrl(config.backendUrl);
+//      }
+
 });
+
+window.app.run(['$rootScope', '$state', '$http', '$templateCache', '$timeout', '$q',
+  function ($rootScope, $state, $http, $templateCache, $timeout, $q) {
+
+    $rootScope.viewModel = {
+      isActivated: function () {
+        return !!this.userProfile;
+      },
+
+      clear: function () {
+        this.userProfile = null;
+      }
+    };
+
+    $rootScope.appState = $state;
+
+    $rootScope.showSpinner = true;
+
+    // Preload partials
+    function fetchPartials() {
+
+      var promises = [];
+
+      [
+        'partials/male/challenges.html',
+        'partials/male/challenge.html'
+
+      ].forEach(function (partial) {
+          var deferred = $q.defer();
+
+          $http.get(partial, {
+            cache: $templateCache
+          })
+            .success(function (data) {
+              deferred.resolve(data);
+            }).
+            error(function (error) {
+              deferred.reject();
+            });
+
+          promises.push(deferred.promise);
+        });
+      return $q.all(promises)
+    }
+
+    fetchPartials().then(function () {
+      $timeout(function () {
+        $rootScope.$broadcast('partials:loaded');
+        console.log('partials:loaded');
+      }, 1000);
+      // need timeout to prevent emitting event before listener is being initialized
+    });
+
+
+    $rootScope.safeDigest = function (scope) {
+      if (scope !== undefined) {
+        try {
+          if (!scope.$$phase && !scope.$$destroyed) {
+            //if scope is not destroyed and actions are not made then digest
+            scope.$digest();
+          }
+        } catch (e) {
+          $rootScope.log.error(e);
+          $rootScope.safeDigest();
+        }
+      }
+      else if (!$rootScope.$$phase) {
+        $rootScope.$digest();
+      }
+    };
+
+  }
+]);
