ed04e3f88f916d1d835e5b3694562b8740b57266
Andrew Meakovski <meako689@gmail.com>
2014-09-14 01:40:14 +0300

    slash

--- server/controllers/login/fb/callback.js
+++ server/controllers/login/fb/callback.js
@@ -16,7 +16,7 @@ module.exports = function (router) {
     router.get('/', 
       passport.authenticate('facebook', { failureRedirect: '/' }),
       function(req, res) {
-        res.redirect('/app');
+        res.redirect('/app/');
     });
     /**
      * Receive the login credentials and authenticate.
