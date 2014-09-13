94b8eafe9bfff5db4ef565a3ab35da56c1ea63ce
Andrew Meakovski <meako689@gmail.com>
2014-09-13 22:12:09 +0300

    cleanup

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,6 @@
 
 var IndexModel = require('../models/index'),
     ProfileModel = require('../models/profile'),
-    AdminModel = require('../models/admin'),
     auth = require('../lib/auth');
 
 
@@ -11,7 +10,6 @@ module.exports = function (router) {
 
     var indexmodel = new IndexModel();
     var profilemodel = new ProfileModel();
-    var adminmodel = new AdminModel();
 
 
     router.get('/', function (req, res) {
@@ -21,11 +19,6 @@ module.exports = function (router) {
 
     router.get('/profile', function(req, res) {
         res.render('profile', profilemodel);
-    });
-
-
-    router.get('/admin', auth.isAuthenticated('admin'), auth.injectUser(), function(req, res) {
-        res.render('admin', adminmodel);
     });
 
     /**
