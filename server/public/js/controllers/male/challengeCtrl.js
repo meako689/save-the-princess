250b5e69e1b03e5fc37f31d2cfda454cd93b06eb
UnforbiddenYet <dmytro.lubenets@gmail.com>
2014-09-14 13:31:45 +0300

    aaaa

--- server/public/js/controllers/male/challengeCtrl.js
+++ server/public/js/controllers/male/challengeCtrl.js
@@ -6,20 +6,27 @@ if (typeof window.angular !== 'undefined') {
     angular.appControllers = angular.module('appControllers', []);
   }
 
-  angular.appControllers.controller('challengeCtrl', ['$scope', '$rootScope', '$state','Restangular', function ($scope, $rootScope, $state, $restangular) {
+  angular.appControllers.controller('challengeCtrl', ['$scope', '$rootScope', '$state','$http', function ($scope, $rootScope, $state, $http) {
 
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-
       if (toState.name === "challenge" && toParams.chId) {
+
         var challengeId = toParams.chId;
         console.log(challengeId);
 
-        $restangular.one("challenge", challengeId).get().then(function(item){
-          $scope.challenge = item;
-        })
+        $http({method: 'GET', url: '/api/challenge/' + challengeId})
+          .success(function(data, status, headers, config) {
+
+            $scope.challenge = data;
+            console.log(data);
+
+        });
+
 
       }
     });
+
+    $scope.h1 = "dsdsd";
 
     $scope.applyChallenge = function(){
       $restangular.one("challenge", challengeId).customGET("apply").then(function(resp){
